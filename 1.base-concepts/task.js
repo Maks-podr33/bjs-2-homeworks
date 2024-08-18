2cc393622c37135be459be2720b1e4f67df792df
maks_podr33 <mixx33@yandex.ru>
2024-08-18 20:40:12 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let equation = b ** 2 - 4 * a * c;
+  if (equation < 0) {
+    arr = [];
+  } else if (equation == 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push(parseFloat(((-b + Math.sqrt(equation)) / (2 * a)).toFixed(2)));
+    arr.push(parseFloat(((-b - Math.sqrt(equation)) / (2 * a)).toFixed(2)));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let totalMortgage = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  return totalMortgage;
 }
